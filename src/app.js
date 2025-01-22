a4b7e2ac807c01fbbd3770c38891cf519246cbcd
riuteshyaaa <riteshyaaa@gmail.com>
2025-01-22 20:28:29 +0530

    implement user management APIs: add user signup, retrieval, feed, and delete functionality

--- src/app.js
+++ src/app.js
@@ -1,59 +1,80 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
 const app = express();
- const User = require("./models/user.js");
+const User = require("./models/user.js");
 
- app.use(express.json());
-
-app.post("/signup", async (req,res) => {
+app.use(express.json());
 
 
-// we have to create a instance of User schema 
-
-const user = new User(req.body);
-
-try{
-  await user.save()
-  res.send("user is added successfully!")
-
-} catch(err){
-  res.status(400).send("Error saving data:"+err.message);
-
-}
-})
-
-//Get One user from the database by their email address 
-//app.get is for to get the user from database 
-app.get("/user", async (req, res) => {
- const userEmail= req.body.email;
+//adding user in database
+app.post("/signup", async (req, res) => {
+  // we have to create a instance of User schema
+  const user = new User(req.body);
   try {
-    const user = await User.findOne({email:userEmail})
-  if(!userEmail)return res.status(404).send("User does not exist");
-  res.send(user);
-  }catch(err){
-    res.status(400).send("Server error: " + err.message)
+    await user.save();
+    res.send("user is added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving data:" + err.message);
   }
-})
+});
 
-//Feed api for getting users from the database 
+//Get One user from the database by their email address
+//app.get is for to get the user from database
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.findOne({ email: userEmail });
+    if (!userEmail) return res.status(404).send("User does not exist");
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Server error: " + err.message);
+  }
+});
 
-app.get("/feed", async (req,res) => {
-  try{
-    const users = await User.find({})
-    if(!users)return res.status(404).send("No user exist");
+
+
+
+//Feed api for getting users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) return res.status(404).send("No user exist");
     res.send(users);
-
-  }catch(err){
-    res.status(400).send("Server error: " +err.message)
+  } catch (err) {
+    res.status(400).send("Server error: " + err.message);
   }
-})
-  
-   
+});
 
+// delete all users from the database
+app.delete("/delete", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.deleteMany({});
+    if (!userEmail) return res.send("User are deleted successfully");
+
+    res.status(404).send("Something went wrong");
+  } catch (err) {
+    res.status(400).send("Server error: " + err.message);
+  }
+});
+
+
+
+//find user by findById method
+app.get("/exists", async (req, res) => {
+
+  try {
+    const user = await User.findById('6790e630fd67aee9a15d3fdf','lastName ');
+    if (!user) return res.status(404).send("User does not exist");
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Server error: " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
-    console.log("Mongoose is connected successfully ")
+    console.log("Mongoose is connected successfully ");
     app.listen(3000, () => {
       console.log("Server is successfully listing at port 3000");
     });
